9da51a164ba40ce6504a0da6c3eef8e85a808413
Larry Liu <mengweiliu600267@gmail.com>
2018-02-08 22:03:08 -0800

    facebook authentication

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '418922855203334',
+        clientSecret: '57d27ee9ea5ccd6d777f9cfc7528e9da'
+    }
 }
